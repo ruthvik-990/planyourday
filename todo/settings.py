bcc3881d86d0ae01227e996026df69ff83d1bdfa
Ruthvik sai <ruthviksai10@gmail.com>
2020-06-02 19:36:17 +0530

    ninth commit

--- todo/settings.py
+++ todo/settings.py
@@ -145,5 +145,5 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER='ruthviksai10@gmail.com'
-EMAIL_HOST_PASSWORD='ruthvikrocks'
+EMAIL_HOST_USER='ruthviktest@gmail.com'
+EMAIL_HOST_PASSWORD='ruthviktestrocks'
